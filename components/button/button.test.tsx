6d91a0292c649c8d012d904b5bf75c98097a2fb5
alessio.marchi <alessio.marchi@accenture.com>
2022-04-01 14:28:57 +0200

    fix: typo in data-testid

--- components/button/button.test.tsx
+++ components/button/button.test.tsx
@@ -3,7 +3,7 @@ import Button from './button'
 
 describe('Button Component', () => {
   it('renders a button', () => {
-    render(<Button data-test-id='button-test'>Button</Button>)
+    render(<Button data-testid='button-test'>Button</Button>)
 
     const button = screen.getByTestId('button-test')
 
