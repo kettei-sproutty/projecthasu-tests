0a1bbc1a3c33621749f390b034d7f803390a6cb5
alessio.marchi <alessio.marchi@accenture.com>
2022-04-01 14:25:39 +0200

    feat: add input error
    - Update styles
    - Add clsx as dependency

--- components/input/input.test.tsx
+++ components/input/input.test.tsx
@@ -2,10 +2,18 @@ import { render, screen } from '@testing-library/react'
 import Input from './input'
 
 describe('Input Component', () => {
-  it('renders a heading', () => {
+  it('renders an input field', () => {
     render(<Input label='Test' name='test' data-testid='input-test' />)
 
     const input = screen.getByTestId('input-test')
+
+    expect(input).toBeInTheDocument()
+  })
+
+  it('should render an error message', () => {
+    render(<Input error={{ type: 'required', message: 'Required' }} label='Test' name='test' />)
+
+    const input = screen.getByTestId('test-error')
 
     expect(input).toBeInTheDocument()
   })
--- components/input/input.tsx
+++ components/input/input.tsx
@@ -1,5 +1,7 @@
 import { forwardRef } from 'react'
+import clsx from 'clsx'
 import type { InputHTMLAttributes } from 'react'
+import type { FieldError } from 'react-hook-form'
 import styles from './input.module.css'
 
 export type InputProps = {
@@ -7,15 +9,31 @@ export type InputProps = {
   label: string
   autoComplete?: string
   id?: string
+  error?: FieldError
 } & InputHTMLAttributes<HTMLInputElement>
 
-const Input = forwardRef<HTMLInputElement, InputProps>(({ name, id, label, ...props }, ref) => {
+const Input = forwardRef<HTMLInputElement, InputProps>(({ name, id, label, error, ...props }, ref) => {
+  const inputClasses = clsx(styles.input, {
+    [styles['input-success']]: !error,
+    [styles['input-error']]: error,
+  })
+
+  const labelClasses = clsx(styles['input-label'], {
+    [styles['input-label--success']]: !error,
+    [styles['input-label--error']]: error,
+  })
+
   return (
     <div>
-      <label htmlFor={name} className={styles['input-label']}>
+      <label htmlFor={name} className={labelClasses}>
         {label}
       </label>
-      <input ref={ref} type='text' name={name} id={id ?? name} className={styles.input} {...props} />
+      <input ref={ref} type='text' name={name} id={id ?? name} className={inputClasses} {...props} />
+      {error && (
+        <p data-testid={`${name}-error`} className={styles['input-error__message']}>
+          {error.message}
+        </p>
+      )}
     </div>
   )
 })
